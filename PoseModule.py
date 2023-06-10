4275b22bf4b1facc1c21d072c18c403c880bec01
g0urav-hustler <gouravchouhan966@gmail.com>
2023-06-10 16:24:37 +0530

    create pose module

---
+++ PoseModule.py
@@ -0,0 +1,31 @@
+import cv2
+import mediapipe as mp
+import math
+
+
+class PoseDetector:
+    """
+    Estimates Pose points of a human body using the mediapipe library.
+    """
+
+    def __init__(self, mode=False, smooth=True,
+                 detectionCon=0.5, trackCon=0.5):
+        """
+        :param mode: In static mode, detection is done on each image: slower
+        :param upBody: Upper boy only flag
+        :param smooth: Smoothness Flag
+        :param detectionCon: Minimum Detection Confidence Threshold
+        :param trackCon: Minimum Tracking Confidence Threshold
+        """
+
+        self.mode = mode
+        self.smooth = smooth
+        self.detectionCon = detectionCon
+        self.trackCon = trackCon
+
+        self.mpDraw = mp.solutions.drawing_utils
+        self.mpPose = mp.solutions.pose
+        self.pose = self.mpPose.Pose(static_image_mode=self.mode,
+                                     smooth_landmarks=self.smooth,
+                                     min_detection_confidence=self.detectionCon,
+                                     min_tracking_confidence=self.trackCon)
